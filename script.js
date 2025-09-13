50bb49042c1e1955c1cea9ce01af5a53a930584e
JethroRendon <jethro.rendon@wvsu.edu.ph>
2025-09-13 18:32:35 +0800

    modifying the html and css file

--- script.js
+++ script.js
@@ -25,4 +25,9 @@ filterButtons.forEach(button => {
   });
 });
 
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
 
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
